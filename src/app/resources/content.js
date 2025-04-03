e40c6357759f466952c3097cebff431fee836392
Sachin Singh <sachinsingh018@gmail.com>
2025-04-03 01:40:08 -0400

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,7 +8,7 @@ const person = {
   },
   role: "Tech consulting",
   avatar: "/images/avatar.jpg",
-  location: "Europe/Vienna", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "Asia/Dubai", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English","Arabic"], // optional: Leave the array empty if you don't want to display languages
 };
 
