99608059ba12081c46762345348187371ef29966
Sachin Singh <sachinsingh018@gmail.com>
2025-04-02 23:58:12 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ export const Footer = () => {
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / {" "}
+            {" "}
             
           </Text>
         </Text>
