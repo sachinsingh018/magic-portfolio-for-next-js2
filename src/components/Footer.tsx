e44d2faa3863b7b47e798deb1f36aedd88ef5a56
Sachin Singh <sachinsingh018@gmail.com>
2025-04-02 23:54:54 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer = () => {
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
           <Text paddingX="4">{person.name}</Text>
-          <Text onBackground="neutral-weak">
+{/*           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
             / Build your portfolio with{" "}
             <SmartLink
@@ -35,7 +35,7 @@ export const Footer = () => {
             >
               Once UI
             </SmartLink>
-          </Text>
+          </Text> */}
         </Text>
         <Flex gap="16">
           {social.map(
