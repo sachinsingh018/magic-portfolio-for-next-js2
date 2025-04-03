e571e5b97729080b2975feb7aecf1a41b7971679
Sachin Singh <sachinsingh018@gmail.com>
2025-04-02 23:50:19 -0400

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -188,8 +188,8 @@ work: {
 
 const blog = {
   label: "Blog",
-  title: "Writing about design and tech...",
-  description: `Read what ${person.name} has been up to recently`,
+  title: "Insights on Innovation, Technology & Beyond",
+  description: `Read what we have been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
 };
